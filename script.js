6ffd141f50fd969aa643722f0dda2178d876a5de
Samuel Muvhango <93419130+muvhangos@users.noreply.github.com>
2025-07-13 01:35:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('greet-btn');
+  button.addEventListener('click', () => {
+    alert('Hope you have a fantastic day!');
+  });
+});
